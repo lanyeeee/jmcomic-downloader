7c036f3971de1dfe3024657976454e434b9c78dc
lanyeeee <1210347077@qq.com>
2024-09-28 20:36:04 +0800

    feat: 添加一些实用函数

---
+++ src-tauri/src/utils.rs
@@ -0,0 +1,18 @@
+pub fn filename_filter(s: &str) -> String {
+    s.chars()
+        .map(|c| match c {
+            '\\' | '/' => ' ',
+            ':' => '：',
+            '*' => '⭐',
+            '?' => '？',
+            '"' => '\'',
+            '<' => '《',
+            '>' => '》',
+            '|' => '丨',
+            '.' => '·',
+            _ => c,
+        })
+        .collect::<String>()
+        .trim()
+        .to_string()
+}
