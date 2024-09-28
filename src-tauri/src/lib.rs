dca646381fa98f0719a91bdbafde8b2b74f522ea
lanyeeee <1210347077@qq.com>
2024-09-28 19:58:45 +0800

    feat: 添加配置文件

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,9 @@
-use tauri::{Context, Wry};
+use anyhow::Context;
+use tauri::{Manager, Wry};
+
+use crate::config::Config;
+
+mod config;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -7,14 +12,21 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn generate_context() -> Context<Wry> {
+#[tauri::command]
+#[specta::specta]
+#[allow(clippy::needless_pass_by_value)]
+fn get_config(config: tauri::State<std::sync::RwLock<Config>>) -> Config {
+    config.read().unwrap().clone()
+}
+
+fn generate_context() -> tauri::Context<Wry> {
     tauri::generate_context!()
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let builder = tauri_specta::Builder::<Wry>::new()
-        .commands(tauri_specta::collect_commands![greet])
+        .commands(tauri_specta::collect_commands![greet, get_config])
         .events(tauri_specta::collect_events![]);
 
     #[cfg(debug_assertions)]
@@ -31,6 +43,19 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(builder.invoke_handler())
+        .setup(move |app| {
+            let app_data_dir = app
+                .path()
+                .app_data_dir()
+                .context("failed to get app data dir")?;
+
+            std::fs::create_dir_all(&app_data_dir)
+                .context(format!("failed to create app data dir: {app_data_dir:?}"))?;
+
+            let config = std::sync::RwLock::new(Config::new(app.handle())?);
+            app.manage(config);
+            Ok(())
+        })
         .run(generate_context())
         .expect("error while running tauri application");
 }
--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -9,13 +9,19 @@ async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsg.value = await commands.greet(name.value);
 }
+
+async function test() {
+  const config = await commands.getConfig();
+  console.log(config);
+}
 </script>
 
 <template>
   <form class="row" @submit.prevent="greet">
-    <input id="greet-input" v-model="name" placeholder="Enter a name..." />
+    <input id="greet-input" v-model="name" placeholder="Enter a name..."/>
     <button type="submit">Greet</button>
   </form>
+  <button @click="test">测试</button>
 
   <p>{{ greetMsg }}</p>
 </template>
