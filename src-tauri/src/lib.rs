0e554100835572d1ba132fcec6d098948555aece
lanyeeee <1210347077@qq.com>
2024-09-28 19:56:39 +0800

    fix: 修复因为宏导致的IDE代码提示缓慢问题

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,6 +7,10 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+fn generate_context() -> Context<Wry> {
+    tauri::generate_context!()
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let builder = tauri_specta::Builder::<Wry>::new()
@@ -27,6 +31,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(builder.invoke_handler())
-        .run(tauri::generate_context!())
+        .run(generate_context())
         .expect("error while running tauri application");
 }
