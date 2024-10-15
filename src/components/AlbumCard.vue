8bda47d9651e66128d1f3f38ae4e4dc9d83e819e
lanyeeee <1210347077@qq.com>
2024-10-16 05:18:32 +0800

    feat: 给漫画卡片的可点击区域添加hover反馈

--- src/components/AlbumCard.vue
+++ src/components/AlbumCard.vue
@@ -28,13 +28,14 @@ async function onClickItem(aid: number) {
   <n-card content-style="padding: 0.25rem;"
           hoverable>
     <div class="flex">
-      <img class="w-24 object-cover mr-4 cursor-pointer"
+      <img class="w-24 object-cover mr-4 cursor-pointer transition-transform duration-200 hover:scale-106"
            :src="`https://cdn-msp3.18comic.vip/media/albums/${albumInfo.id}_3x4.jpg`"
            alt=""
            referrerpolicy="no-referrer"
            @click="onClickItem(parseInt(albumInfo.id))"/>
       <div class="flex flex-col h-full">
-        <span class="font-bold text-xl line-clamp-3 cursor-pointer" @click="onClickItem(parseInt(albumInfo.id))">
+        <span class="font-bold text-xl line-clamp-3 cursor-pointer transition-colors duration-200 hover:text-blue-5"
+              @click="onClickItem(parseInt(albumInfo.id))">
           {{ albumInfo.name }}
         </span>
         <span class="text-red">作者：{{ albumInfo.author }}</span>
