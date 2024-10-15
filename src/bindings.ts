b855127f2cde5361839b47067310199d9da18397
lanyeeee <1210347077@qq.com>
2024-10-16 04:58:52 +0800

    feat: 后端支持下载某部漫画的所有章节

--- src/bindings.ts
+++ src/bindings.ts
@@ -83,6 +83,14 @@ async downloadChapters(chapterInfos: ChapterInfo[]) : Promise<Result<null, Comma
     else return { status: "error", error: e  as any };
 }
 },
+async downloadAlbum(aid: number) : Promise<Result<null, CommandError>> {
+    try {
+    return { status: "ok", data: await TAURI_INVOKE("download_album", { aid }) };
+} catch (e) {
+    if(e instanceof Error) throw e;
+    else return { status: "error", error: e  as any };
+}
+},
 async showPathInFileManager(path: string) : Promise<Result<null, CommandError>> {
     try {
     return { status: "ok", data: await TAURI_INVOKE("show_path_in_file_manager", { path }) };
