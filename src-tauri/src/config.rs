f27b0cc778a3009746a1479fdb0cc5d17fbf7750
lanyeeee <lanyeeeeee@gmail.com>
2024-11-13 06:07:12 +0800

    feat: 后端支持选择保存格式

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use crate::types::DownloadFormat;
+use crate::types::{ArchiveFormat, DownloadFormat};
 use serde::{Deserialize, Serialize};
 use specta::Type;
 use tauri::{AppHandle, Manager};
@@ -12,17 +12,20 @@ pub struct Config {
     pub password: String,
     pub download_dir: PathBuf,
     pub download_format: DownloadFormat,
+    pub archive_format: ArchiveFormat,
 }
 
 impl Config {
     pub fn new(app: &AppHandle) -> anyhow::Result<Self> {
         let app_data_dir = app.path().app_data_dir()?;
         let config_path = app_data_dir.join("config.json");
+        // TODO: 实现Default trait以替换下面的代码
         let default_config = Config {
             username: String::new(),
             password: String::new(),
             download_dir: app_data_dir.join("漫画下载"),
             download_format: DownloadFormat::Jpeg,
+            archive_format: ArchiveFormat::Image,
         };
         // 如果配置文件存在且能够解析，则使用配置文件中的配置，否则使用默认配置
         let config = if config_path.exists() {
