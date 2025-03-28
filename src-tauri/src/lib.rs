0e8cb19138ac02e20c00d9c9eaefb4094d408c59
lanyeeee <1210347077@qq.com>
2025-03-28 18:19:48 +0800

    feat: 用`tauri-plugin-opener`替换`tauri-plugin-shell`和`path-slash`

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -65,7 +65,7 @@ pub fn run() {
 
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_opener::init())
         .invoke_handler(builder.invoke_handler())
         .setup(move |app| {
             builder.mount_events(app);
