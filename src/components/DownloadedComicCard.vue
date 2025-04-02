c6ff13de2715608ec69f0ef16647175a0b0b337b
lanyeeee <lanyeeeeee@gmail.com>
2025-04-02 20:41:17 +0800

    feat: 支持导出cbz和pdf

--- src/components/DownloadedComicCard.vue
+++ src/components/DownloadedComicCard.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import { Comic } from '../bindings.ts'
+import { Comic, commands } from '../bindings.ts'
 import { CurrentTabName } from '../types.ts'
+import { useNotification } from 'naive-ui'
+
+const notification = useNotification()
 
 const props = defineProps<{
   comic: Comic
@@ -13,6 +16,22 @@ function pickComic() {
   pickedComic.value = props.comic
   currentTabName.value = 'chapter'
 }
+
+async function exportCbz() {
+  const result = await commands.exportCbz(props.comic)
+  if (result.status === 'error') {
+    notification.error({ title: '导出cbz失败', description: result.error })
+    return
+  }
+}
+
+async function exportPdf() {
+  const result = await commands.exportPdf(props.comic)
+  if (result.status === 'error') {
+    notification.error({ title: '导出pdf失败', description: result.error })
+    return
+  }
+}
 </script>
 
 <template>
@@ -24,14 +43,16 @@ function pickComic() {
         alt=""
         referrerpolicy="no-referrer"
         @click="pickComic" />
-      <div class="flex flex-col w-full justify-between">
-        <div class="flex flex-col">
-          <span
-            class="font-bold text-xl line-clamp-3 cursor-pointer transition-colors duration-200 hover:text-blue-5"
-            @click="pickComic">
-            {{ comic.name }}
-          </span>
-          <span class="text-red">作者：{{ comic.author }}</span>
+      <div class="flex flex-col w-full">
+        <span
+          class="font-bold text-xl line-clamp-3 cursor-pointer transition-colors duration-200 hover:text-blue-5"
+          @click="pickComic">
+          {{ comic.name }}
+        </span>
+        <span class="text-red">作者：{{ comic.author }}</span>
+        <div class="flex ml-auto mt-auto gap-col-2">
+          <n-button size="tiny" @click="exportCbz">导出cbz</n-button>
+          <n-button size="tiny" @click="exportPdf">导出pdf</n-button>
         </div>
       </div>
     </div>
