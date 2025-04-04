15a34d0944079eed8b711269abe5595319974b87
lanyeeee <1210347077@qq.com>
2025-04-04 08:46:28 +0800

    feat: 优化配置文件解析逻辑，支持配置项的向后兼容

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use crate::types::{DownloadFormat, ProxyMode};
 use serde::{Deserialize, Serialize};
@@ -23,24 +23,18 @@ impl Config {
     pub fn new(app: &AppHandle) -> anyhow::Result<Self> {
         let app_data_dir = app.path().app_data_dir()?;
         let config_path = app_data_dir.join("config.json");
-        // TODO: 实现Default trait以替换下面的代码
-        let default_config = Config {
-            username: String::new(),
-            password: String::new(),
-            download_dir: app_data_dir.join("漫画下载"),
-            export_dir: app_data_dir.join("漫画导出"),
-            download_format: DownloadFormat::Jpeg, // TODO: 给DownloadFormat实现 Default trait
-            proxy_mode: ProxyMode::default(),
-            proxy_host: String::new(),
-            proxy_port: 7890,
-            enable_file_logger: true,
-        };
-        // 如果配置文件存在且能够解析，则使用配置文件中的配置，否则使用默认配置
+
         let config = if config_path.exists() {
             let config_string = std::fs::read_to_string(config_path)?;
-            serde_json::from_str(&config_string).unwrap_or(default_config)
+            match serde_json::from_str(&config_string) {
+                // 如果能够直接解析为Config，则直接返回
+                Ok(config) => config,
+                // 否则，将默认配置与文件中已有的配置合并
+                // 以免新版本添加了新的配置项，用户升级到新版本后，所有配置项都被重置
+                Err(_) => Config::merge_config(&config_string, &app_data_dir),
+            }
         } else {
-            default_config
+            Config::default(&app_data_dir)
         };
         config.save(app)?;
         Ok(config)
@@ -52,5 +46,41 @@ impl Config {
         let config_string = serde_json::to_string_pretty(self)?;
         std::fs::write(config_path, config_string)?;
         Ok(())
+    }
+
+    fn merge_config(config_string: &str, app_data_dir: &Path) -> Config {
+        let Ok(mut json_value) = serde_json::from_str::<serde_json::Value>(config_string) else {
+            return Config::default(app_data_dir);
+        };
+        let serde_json::Value::Object(ref mut map) = json_value else {
+            return Config::default(app_data_dir);
+        };
+        let Ok(default_config_value) = serde_json::to_value(Config::default(app_data_dir)) else {
+            return Config::default(app_data_dir);
+        };
+        let serde_json::Value::Object(default_map) = default_config_value else {
+            return Config::default(app_data_dir);
+        };
+        for (key, value) in default_map {
+            map.entry(key).or_insert(value);
+        }
+        let Ok(config) = serde_json::from_value(json_value) else {
+            return Config::default(app_data_dir);
+        };
+        config
+    }
+
+    fn default(app_data_dir: &Path) -> Config {
+        Config {
+            username: String::new(),
+            password: String::new(),
+            download_dir: app_data_dir.join("漫画下载"),
+            export_dir: app_data_dir.join("漫画导出"),
+            download_format: DownloadFormat::Jpeg, // TODO: 给DownloadFormat实现 Default trait
+            proxy_mode: ProxyMode::default(),
+            proxy_host: "127.0.0.1".to_string(),
+            proxy_port: 7890,
+            enable_file_logger: true,
+        }
     }
 }
